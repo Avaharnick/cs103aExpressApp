47ac4113f45346a041a547881d7b5ff87c5d8fd1
Avaharnick <64444623+Avaharnick@users.noreply.github.com>
2022-05-05 00:13:21 -0400

    env variables

--- app.js
+++ app.js
@@ -32,8 +32,10 @@ const Player = require('./models/Player')
 // *********************************************************** //
 
 const mongoose = require( 'mongoose' );
+
+const mongodb_URI = process.env.mongodb_URI 
 //const mongodb_URI = 'mongodb://localhost:27017/cs103a_todo'
-const mongodb_URI = 'mongodb+srv://Avanicole:Avanicole15!@cluster0.x77cx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// const mongodb_URI = 'mongodb+srv://Avanicole:Avanicole15!@cluster0.x77cx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 //mongodb+srv://cs103a:<password>@cluster0.kgugl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
 mongoose.connect( mongodb_URI, { useNewUrlParser: true, useUnifiedTopology: true } );
